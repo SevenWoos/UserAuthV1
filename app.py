72753bea478a36417f7a3e5e7d23d364cf8407f6
7Woos <swenwu96@gmail.com>
2025-07-15 00:04:00 -0700

    added admin feature

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(
