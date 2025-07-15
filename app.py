d804e55e588e7989a73da7fb880663df195b698d
7Woos <swenwu96@gmail.com>
2025-07-14 21:25:01 -0700

    set up user db and register and login forms

--- app.py
+++ app.py
@@ -1,18 +1,68 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+db = SQLAlchemy(app)
+
+# Database Tables
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20
+    )], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=4, max=20
+    )], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField("Register")
+
+    # Check for duplicate usernames
+    def validate_username(self, username):
+        # Query database for the username
+        existing_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_username:
+            raise ValidationError(
+                "That username already exists. Please choose a different one!"
+            )
+  
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20
+    )], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=4, max=20
+    )], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField("Login")
+
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    form = RegisterForm()
+    return render_template('register.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
